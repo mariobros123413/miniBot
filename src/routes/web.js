045805d45701632e10f211e67be213e75bc7f589
José Mario Herbas Miranda <j.mario18@hotmail.es>
2022-08-28 11:48:11 -0400

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,7 +7,7 @@ let initWebRoutes = (app) =>{
     router.get("/", miniBotController.test);
 
     router.get("/webhook", miniBotController.getWebhook);
-    router.get("/webhook", miniBotController.postWebhook);
+    router.post("/webhook", miniBotController.postWebhook);
 
     return app.use("/", router);
 };
